893def229730bde51c9d16a8c8329b0d58c4f5da
Marya <marya.vadymovna@gmail.com>
2021-07-10 21:29:12 +0200

    homeIntro, stages component added

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,26 @@
 <template>
 	<div>
-		<h2>ПРивет мир</h2>
-		<VideoItem />
+		<template v-if="!$fetchState.pending">
+			<div v-for="(slice, i) in slices" :key="slice.slice_type + i">
+				<!-- <HomeIntro v-if="slice.slice_type == 'homeIntro'" :key="slice.slice_type + i" :data="slice" /> -->
+				<Stages v-if="slice.slice_type == 'stages'" :data="slice" />
+			</div>
+		</template>
+		<template v-else>
+			<h2>Loading...</h2>
+		</template>
 	</div>
 </template>
 
 <script>
 export default {
+	data: () => ({
+		slices: [],
+	}),
 	async fetch() {
-		const headerData = await this.$prismic.api.getSingle('index')
-		console.log(headerData)
+		const fetch = await this.$prismic.api.getSingle('index')
+		console.log(fetch.data.body)
+		this.slices = fetch.data.body
 	},
 }
 </script>
