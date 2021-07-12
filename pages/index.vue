1b1f798d34ff3c225714383f39bb17651771963c
Marya <marya.vadymovna@gmail.com>
2021-07-12 15:37:44 +0200

    partners component desktop , mobile done

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="page">
+	<div>
 		<template v-if="!$fetchState.pending">
 			<div v-for="(slice, i) in slices" :key="slice.slice_type + i">
 				<HomeIntro v-if="slice.slice_type == 'homeintro'" :key="slice.slice_type + i" :data="slice" />
@@ -20,7 +20,9 @@
 </template>
 
 <script>
+import Partners from '~/components/sections/Partners.vue'
 export default {
+	components: { Partners },
 	data: () => ({
 		slices: [],
 	}),
@@ -33,7 +35,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.page {
+.container {
 	width: 100%;
 	display: flex;
 	flex-direction: column;
